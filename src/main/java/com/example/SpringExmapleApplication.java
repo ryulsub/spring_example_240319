eb97d27cd1530c8adb494d8bd44505bd3ff40a58
DESKTOP-E4VTSLN\user <test@test.com>
2024-06-12 17:22:57 +0900

    초기환경 설정

---
+++ src/main/java/com/example/SpringExmapleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExmapleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExmapleApplication.class, args);
+	}
+
+}
