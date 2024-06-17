e00aaaceee500d153f0e281e9e89cbd5e7c6813f
DESKTOP-E4VTSLN\user <test@test.com>
2024-06-17 18:50:04 +0900

    240617

--- src/main/java/com/example/lesson03/mapper/ReviewMapper.java
+++ src/main/java/com/example/lesson03/mapper/ReviewMapper.java
@@ -1,12 +1,27 @@
 package com.example.lesson03.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.lesson03.domain.Review;
 
 @Mapper
 public interface ReviewMapper {
 	
-	// input:X  output:Review or null
+	// input:id  output:Review or null
 	public Review selectReviewById(int id);
+	
+	public int insertReview(Review review);
+	
+	// @Param 하나의 맵으로 xml에 전송
+	public int insertReviewAsField(
+			@Param("storeId") int storeId, 
+			@Param("menu") String menu,
+			@Param("userName") String userName, 
+			@Param("point") Double point, 
+			@Param("review") String review);
+	
+	public int updateReviewById(
+			@Param("id") int id, 
+			@Param("review") String review);
 }
