0a60dabedce097820bda2345c098d67cea100705
DESKTOP-E4VTSLN\user <test@test.com>
2024-06-20 15:40:32 +0900

    240620

--- src/main/java/com/example/lesson04/Lesson04Ex01Controller.java
+++ src/main/java/com/example/lesson04/Lesson04Ex01Controller.java
@@ -2,6 +2,7 @@ package com.example.lesson04;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.example.lesson04.bo.UserBO;
 import com.example.lesson04.domain.User;
 
-import ch.qos.logback.core.model.Model;
-
 @RequestMapping("/lesson04/ex01")
 @Controller // html 화면의 경우 @ResponseBody 사용안함
 public class Lesson04Ex01Controller {
 	
+	@Autowired
+	private UserBO userBO;
+
 	// 회원가입 화면
 	// http://localhost/lesson04/ex01/add-user-view
 	@RequestMapping(path = "/add-user-view", method = RequestMethod.GET)
@@ -24,10 +26,7 @@ public class Lesson04Ex01Controller {
 		return "lesson04/addUser"; // html view 경로
 	}
 	
-	@Autowired
-	private UserBO userBO;
-	
-	// 회원가입 진행 => DB 저장 => 결과화면
+	// 회원가입 진행 => DB 저장 => 결과 화면
 	@PostMapping("/add-user")
 	public String addUser(
 			@RequestParam("name") String name,
@@ -45,16 +44,15 @@ public class Lesson04Ex01Controller {
 	// 최근 가입자 뿌리는 화면
 	// http://localhost/lesson04/ex01/latest-user-view
 	@GetMapping("/latest-user-view")
-	public String latestUserView(Model model) { // Model: view화면에서 
+	public String latestUserView(Model model) { // Model: view 화면에서 데이터를 꺼내쓸 수 있는 객체
 		// DB select (최신 가입자 1명)
 		User user = userBO.getLatestUser();
 		
 		// Model 주머니에 담는다. => HTML에서 꺼내쓴다.
 		model.addAttribute("result", user);
-		model.addAttribute("title","최근 가입한 유저 정보!");
+		model.addAttribute("title", "최근 가입한 유저 정보!");
 		
 		// 화면 이동
 		return "lesson04/latestUser";
 	}
-	
 }
--- src/main/java/com/example/lesson04/bo/StudentBO.java
+++ src/main/java/com/example/lesson04/bo/StudentBO.java
@@ -4,17 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.lesson04.domain.Student;
+import com.example.lesson04.mapper.StudentMapper;
 
 @Service
 public class StudentBO {
 	
 	@Autowired
 	private StudentMapper studentMapper;
-	
+
 	// input: Student
 	// output: X
 	public void addStudent(Student student) {
-		
-		
+		studentMapper.insertStudent(student);
 	}
 }
