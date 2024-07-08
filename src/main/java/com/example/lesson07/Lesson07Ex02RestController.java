f60d827f61be18b6b475c7e5a1a53727cb8f6776
DESKTOP-E4VTSLN\user <test@test.com>
2024-07-08 18:50:36 +0900

    240708

--- src/main/java/com/example/lesson07/Lesson07Ex02RestController.java
+++ src/main/java/com/example/lesson07/Lesson07Ex02RestController.java
@@ -48,8 +48,14 @@ public class Lesson07Ex02RestController {
 		
 		// 9) id가 2 ~ 5까지 조회  between
 		return studentRepository.findByIdBetween(2, 5);
-		
+	}
+	
+	@GetMapping("/selet2")
+	public List<StudentEntity> select2() {
+		// 장래희망이 개발자인 데이터 조회
+		return studentRepository.findByDreamJob("개발자");
 		
 		
 	}
+	
 }
